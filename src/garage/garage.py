95ff81e9b51b29b0da024954fc624d2e0a316b43
Justin Larkin <jlarkin9@gmail.com>
2021-02-19 05:02:12 -0500

    Simplified parking algorithm to reduce complexity...Almost Done!

--- src/garage/garage.py
+++ src/garage/garage.py
@@ -9,7 +9,7 @@ class Garage:
     def __init__(self, levels: list = None):
         self.levels = levels or []
 
-    def add_vehicles(self, vehicles: list = None) -> list:
+    def add_vehicles(self, vehicle_queue: list = None) -> list:
         handicap_spaces = deque()
         premium_spaces = deque()
         compact_spaces = deque()
@@ -26,60 +26,41 @@ class Garage:
                     regular_spaces.append(space)
 
         rejected_vehicles = list()
-        #sv = sorted(vehicles, key=lambda x: x.permit.value, reverse=True)
-        for vehicle in vehicles:
-            if vehicle.permit == (Permit.DISABILITY.value | Permit.PREMIUM.value):
-                if handicap_spaces:
-                    handicap_spaces.popleft().vehicle = vehicle
-                    continue
-                elif premium_spaces:
-                    premium_spaces.popleft().vehicle = vehicle
-                    continue
-                elif vehicle.vehicle_type == VehicleType.Compact and compact_spaces:
-                    compact_spaces.popleft().vehicle = vehicle
-                    continue
-                elif regular_spaces:
-                    regular_spaces.popleft().vehicle = vehicle
-                    continue
+        vehicles_sorted_by_permit = sorted(vehicle_queue, key=lambda x: x.permit.value, reverse=True)
 
-            if vehicle.permit == Permit.DISABILITY:
-                if handicap_spaces:
-                    handicap_spaces.popleft().vehicle = vehicle
-                    continue
-                elif compact_spaces and vehicle.vehicle_type == VehicleType.Compact:
-                    compact_spaces.popleft().vehicle = vehicle
-                    continue
-                elif regular_spaces:
-                    regular_spaces.popleft().vehicle = vehicle
-                    continue
+        while vehicle_queue:
+            vehicle = vehicle_queue.pop()
+            premium_reservation_vehicles = list()
+            general_parking = list()
 
-            if vehicle.permit == Permit.PREMIUM:
+            dual = (vehicle.permit == (Permit.DISABILITY.value | Permit.PREMIUM.value))
+            if not (handicap_spaces or premium_spaces or compact_spaces) and regular_spaces and vehicle.permit != Permit.PREMIUM and vehicle.permit != dual:
+                general_parking.append(vehicle)
+                continue
+
+            if vehicle.permit == dual or Permit.DISABILITY and handicap_spaces:
+                handicap_spaces.popleft().vehicle = vehicle
+                continue
+
+            if vehicle.permit == dual or Permit.PREMIUM:
                 if premium_spaces:
                     premium_spaces.popleft().vehicle = vehicle
                     continue
-                elif compact_spaces and vehicle.vehicle_type == VehicleType.Compact:
-                    compact_spaces.popleft().vehicle = vehicle
-                    continue
                 elif regular_spaces:
-                    regular_spaces.popleft().vehicle = vehicle
+                    premium_reservation_vehicles.append(vehicle)
                     continue
 
-            if vehicle.vehicle_type == VehicleType.Compact:
-                if compact_spaces:
+            if vehicle.vehicle_type == VehicleType.Compact and compact_spaces:
                     compact_spaces.popleft().vehicle = vehicle
+                    vehicle_queue.remove(vehicle)
                     continue
-                elif regular_spaces:
-                    regular_spaces.popleft().vehicle = vehicle
-                    continue
-
-            if regular_spaces:
-                regular_spaces.popleft().vehicle = vehicle
-                continue
 
             rejected_vehicles.append(vehicle)
 
 
-        #return list(set(rejected_vehicles).difference(vehicles))
-        return rejected_vehicles
+        premium_reservation_vehicles.append(general_parking)
+
+
+        return vehicles
 
 
